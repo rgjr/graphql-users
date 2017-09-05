e4dac299224f1d7fe7df99d69be509a4e839e90e
Raul Gutierrez <raul@rauls-mbp.lan>
2017-09-05 15:36:10 -0700

    added db file for json-server, added schema and mutations

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const expressGraphQL = require('express-graphql');
-const schema = require('./schema/schema');
+const express = require("express");
+const expressGraphQL = require("express-graphql");
+const schema = require("./schema/schema");
 const app = express();
 
 app.use(
-  '/graphql',
+  "/graphql",
   expressGraphQL({
     schema,
     graphiql: true
@@ -12,5 +12,5 @@ app.use(
 );
 
 app.listen(4000, () => {
-  console.log('Listening');
+  console.log("Listening");
 });
